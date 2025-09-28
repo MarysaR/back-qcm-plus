99e6d39dc87edf6a503cf36d4fe224fc54cf0531
MathieuHeitz <mheitzmann1@hotmail.fr>
2025-09-28 09:22:13 +0200

    delete all try/catch

--- src/repositories/user-prisma-repository.ts
+++ src/repositories/user-prisma-repository.ts
@@ -8,6 +8,7 @@ import {
   TechnicalError,
   User,
   UserRepository,
+  ValidationError,
 } from 'logic-qcm-plus';
 import prisma from '../config/prisma';
 
@@ -72,8 +73,9 @@ export class UserPrismaRepository implements UserRepository {
 
   async createUser(user: User): Promise<Ok<void, AppError> | Err<void, AppError>> {
     console.log('createUser called with:', user);
-    try {
-      await prisma.appUser.create({
+  
+    const result = await prisma.appUser
+      .create({
         data: {
           first_name: user.firstName,
           last_name: user.lastName,
@@ -82,19 +84,33 @@ export class UserPrismaRepository implements UserRepository {
           company: user.company,
           email: user.email,
           role: {
-            connect: { id_role: user.roleId }
+            connect: { id_role: user.roleId },
           },
           is_active: user.isActive,
           created_at: user.createdAt,
           updated_at: user.updatedAt,
+        },
+      })
+      .then(() => Ok.of(undefined))
+      .catch((error: any) => {
+        console.error('Erreur Prisma lors de la création du user :', error);
+  
+        if (error.code === 'P2002') {
+          return Err.of(
+            new ValidationError(
+              `Un utilisateur avec cet email ou login existe déjà.`
+            )
+          );
         }
+  
+        return Err.of(
+          new TechnicalError(
+            `Erreur lors de la création de l'utilisateur : ${error.message}`
+          )
+        );
       });
   
-      return Ok.of(undefined);
-    } catch (error: any) {
-      console.error('Erreur Prisma lors de la création du user :', error);
-      return Err.of(new TechnicalError('Erreur lors de la création de l\'utilisateur'));
-    }
+    return result as Ok<void, AppError> | Err<void, AppError>;
   }
   
 }
