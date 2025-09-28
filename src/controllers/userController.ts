61934cd9df34e8354237c367db597af7742a1661
MathieuHeitz <mheitzmann1@hotmail.fr>
2025-09-28 09:56:24 +0200

    remove magic number and remplace by http_status

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { GetUsersUseCase, CreateUserUseCase, RoleEnum } from 'logic-qcm-plus';
+import { GetUsersUseCase, CreateUserUseCase, RoleEnum, HTTP_STATUS } from 'logic-qcm-plus';
 import { UserPrismaRepository } from '../repositories/user-prisma-repository';
 import { ValidationError, AlreadyExistError } from 'logic-qcm-plus';
 
@@ -11,13 +11,11 @@ export class UserController {
   public getUsers(req: Request, res: Response): void {
     const result = this.getUsersUseCase.execute();
     if (result.isOk()) {
-      res.status(200).json({ users: result.value });
+      res.status(HTTP_STATUS.OK).json({ users: result.value });
     } else {
-      res.status(404).json({ error: result.error.message });
+      res.status(HTTP_STATUS.NOT_FOUND).json({ error: result.error.message });
     }
   }
-
-
 
   public async createUser(req: Request, res: Response): Promise<void> {
     console.log('Requête reçue dans controlleur:', req.body);
@@ -28,7 +26,7 @@ export class UserController {
   //todo: supprimer nombre magique remplacer par http-status-codes
 
     if (!curentUserRoleId || curentUserRoleId !== 1) {
-      res.status(403).json({ error: 'Accès interdit : rôle ADMIN requis.' });
+      res.status(HTTP_STATUS.FORBIDDEN).json({ error: 'Accès interdit : rôle ADMIN requis.' });
       return;
     }
   
@@ -44,12 +42,12 @@ export class UserController {
     const result = await this.createUserUseCase.createUser(curentUserRoleId, user);
   
     if (result) {
-      res.status(201).json(result);
+      res.status(HTTP_STATUS.CREATED).json(result);
     } else {
       if (result instanceof ValidationError || result instanceof AlreadyExistError) {
-        res.status(400).json({ error: result.message });
+        res.status(HTTP_STATUS.BAD_REQUEST).json({ error: result.message });
       } else {
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ error: 'Internal server error' });
       }
     }
   }
