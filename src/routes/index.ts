a2a7339c4dd2bbbe6eea352ef6983525cac58833
neycha <regentmarysapro@gmail.com>
2025-10-03 12:06:06 +0200

    refacto: delete old implementation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import authRouter from './auth/auth-route';
-import pingRouter from './ping-route';
 import userRouter from './user-route';
 
 const router = Router();
 
 router.use('/auth', authRouter);
-router.use(pingRouter);
 router.use(userRouter);
 
 export default router;
