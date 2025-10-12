a1355092eca41e1d79800ffdbac053431f44fd44
MathieuHeitz <mheitzmann1@hotmail.fr>
2025-10-12 17:12:37 +0200

    fix back

--- src/controllers/user/userController.ts
+++ src/controllers/user/userController.ts
@@ -1,40 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response } from 'express';
 import {
+  AppError,
   CreateUserCommand,
   CreateUserUseCase,
-  PasswordHasher,
+  Err,
+  Ok,
+  PermissionDeniedError,
+  Result,
+  ValidationError,
 } from 'logic-qcm-plus';
-import { UserPrismaRepository } from '../../repositories/user-prisma-repository';
-import { HTTP_STATUS } from '../../constants/httpStatus';
-import { BcryptPasswordHasher } from '../../providers/bcryptPasswordHasher';
+import { CatchErrors } from '../../utils/catchErrors';
+import { HandleResult } from '../../utils/handleResult';
 import { claimsToUser } from '../../utils/claimsToUser';
+import { UserPrismaRepository } from '../../repositories/user-prisma-repository';
+import { BcryptPasswordHasher } from '../../providers/bcryptPasswordHasher';
 
 export class UserController {
-  private userRepository = new UserPrismaRepository();
-  private passwordHasher: PasswordHasher = new BcryptPasswordHasher();
-  private createUserUseCase = new CreateUserUseCase(
-    this.userRepository,
-    this.passwordHasher
-  );
-
-  public async createUser(req: Request, res: Response): Promise<void> {
+  @CatchErrors()
+  @HandleResult()
+  async createUser(
+    req: Request,
+    res: Response
+  ): Promise<Result<void, AppError>> {
     const newUser = req.body;
+    const currentUser = claimsToUser(req.claims);
 
-    const currentUser = claimsToUser(req.claims).roleId;
+    if (!currentUser) {
+      return Err.of(new PermissionDeniedError('Utilisateur non authentifié'));
+    }
+
+    if (!newUser) {
+      return Err.of(new ValidationError('Données utilisateur invalides'));
+    }
+
+    const createUserUseCase = new CreateUserUseCase(
+      new UserPrismaRepository(),
+      new BcryptPasswordHasher()
+    );
 
     const command: CreateUserCommand = {
+      currentUser: currentUser.id,
       newUser,
-      currentUser,
     };
 
-    const result = await this.createUserUseCase.execute(command);
-
+    const result = await createUserUseCase.execute(command);
     if (result.isErr()) {
-      res.status(HTTP_STATUS.BAD_REQUEST).json({ error: result.error.message });
-    } else {
-      res
-        .status(HTTP_STATUS.CREATED)
-        .json({ message: 'Utilisateur créé avec succès' });
+      return Err.of(result.error);
     }
+
+    return Ok.of(undefined);
   }
 }
